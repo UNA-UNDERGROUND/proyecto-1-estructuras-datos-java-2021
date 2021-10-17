b4b184a30161899d3b53553af3bcd064c88e631a
Jonathan Guzmán <jonguz5@hotmail.com>
2021-10-16 18:40:22 -0600

    commit inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proyecto 1 estructuras de datos Java
+
+implementacion de una matriz en Java
+
+el proyecto usa maven y JUNIT con el JDK 16
---
+++ src/test/java/cr/ac/una/AppTest.java
@@ -0,0 +1,19 @@
+package cr.ac.una;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    @BeforeEach
+    public void init() {
+    }
+
+    @Test
+    public void prueba1() {
+    }
+}
+
