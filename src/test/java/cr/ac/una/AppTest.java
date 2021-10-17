19ad166cf34ff4930905c3cd29bf519a3a1b2f95
Jonathan Guzmán <jonguz5@hotmail.com>
2021-10-16 18:44:17 -0600

    corregido test

--- src/test/java/cr/ac/una/AppTest.java
+++ src/test/java/cr/ac/una/AppTest.java
@@ -1,8 +1,8 @@
 package cr.ac.una;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
